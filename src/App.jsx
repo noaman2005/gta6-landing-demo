f118a3c8f11684b0bb6a2b8d40d76d617247ae8a
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2025-06-19 19:08:59 +0200

    Implement Jason

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger } from 'gsap/all';
 import NavBar from './sections/NavBar';
 import Hero from './sections/Hero';
 import FirstVideo from './sections/FirstVideo';
+import Jason from './sections/Jason';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -13,6 +14,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <FirstVideo />
+      <Jason />
     </main>
   )
 }
