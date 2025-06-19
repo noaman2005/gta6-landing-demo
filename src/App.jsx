fc8d572efa404de5c1d2f6d732e8da08fb8b042c
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2025-06-19 19:52:00 +0200

    Implement Lucia

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './sections/Hero';
 import FirstVideo from './sections/FirstVideo';
 import Jason from './sections/Jason';
 import SecondVideo from './sections/SecondVideo';
+import Lucia from './sections/Lucia';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -19,7 +20,7 @@ const App = () => {
       <Jason />
 
       <SecondVideo />
-      {/* <Lucia /> */}
+      <Lucia />
     </main>
   )
 }
