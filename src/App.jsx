4c64c7fdff250e216db792a07c7f41416fd7cb75
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2025-06-20 11:19:02 +0200

    Implement Postcard

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FirstVideo from './sections/FirstVideo';
 import Jason from './sections/Jason';
 import SecondVideo from './sections/SecondVideo';
 import Lucia from './sections/Lucia';
+import PostCard from './sections/PostCard';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -21,6 +22,8 @@ const App = () => {
 
       <SecondVideo />
       <Lucia />
+
+      <PostCard />
     </main>
   )
 }
