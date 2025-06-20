fadc685abf346a2d327b14552c041154cdf7a9d6
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2025-06-20 11:50:16 +0200

    Implement Outro

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SecondVideo from './sections/SecondVideo';
 import Lucia from './sections/Lucia';
 import PostCard from './sections/PostCard';
 import Final from './sections/Final';
+import Outro from './sections/Outro';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -26,6 +27,7 @@ const App = () => {
 
       <PostCard />
       <Final />
+      <Outro />
     </main>
   )
 }
